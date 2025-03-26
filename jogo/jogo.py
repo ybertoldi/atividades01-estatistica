f9747caca08151ebc6101e78902b94df1799344a
Yuri <129873996+ybertoldi@users.noreply.github.com>
2025-03-26 10:03:46 -0300

    Update jogo.py

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -1,5 +1,7 @@
 import random
 from colorama import Fore
+# use ">pip install colorama" ou simplesmente apague o import.
+# se estiver no vscode, adicione  "code-runner.runInTerminal": true no settings.json
 
 def mostraTabuleiro(tabuleiro, fila_jogador, casa_jogador):
     
@@ -23,19 +25,26 @@ def jogarDado():
     return random.randint(1,6)
 
 def jogada():
-    global tabuleiro, fila_jogador, casa_jogador, pontuacao
-    print("---------------------------------------------------------------------------------")    
+    global tabuleiro, fila_jogador, casa_jogador, pontuacao   
     n = jogarDado()
-    print(f"você tirou {n}!\n")
+    print(f"você tirou {n}!")
     
     if casa_jogador < tabuleiro[fila_jogador]:
         casa_jogador = min(casa_jogador + n, tabuleiro[fila_jogador])
     else:
         if n == 4:
+            print("você passou para a próxima fila!")
             fila_jogador = 0 if fila_jogador + 1 >= len(tabuleiro) else fila_jogador+1
             if (fila_jogador == 0):
                 pontuacao += 1
+                print("você deu uma volta no tabuleiro!")
+        else:
+            print("você voltou para o começo da fila!")
         casa_jogador = 1
+        
+    pontuacao_fracionada = pontuacao + (fila_jogador / len(tabuleiro))
+    print(f"pontuação atual: {pontuacao} | pontuação fracionada: {pontuacao_fracionada:.2f}")
+    print()
         
 
 tabuleiro = [9, 8, 10, 12, 7, 5]
@@ -48,17 +57,23 @@ mostraTabuleiro(tabuleiro, fila_jogador, casa_jogador)
 t = input("quantas jogadas você que fazer? ")
 
 for i in range(int(t)):
+    print("---------------------------------------------------------------------------------") 
+    print(f"JOGADA {i+1}")
     print("---------------------------------------------------------------------------------")
-    print(f"JOGADA {i}")
+    
     jogada()
     mostraTabuleiro(tabuleiro, fila_jogador, casa_jogador)
 
 
 ### se quiser fazer cada jogada com enter ###
-#     
+
 # t = input("aperte enter para rodar um dado (\"q\" para sair)") 
-#
+# i = 1
 # while t != "q":
+#     print("---------------------------------------------------------------------------------") 
+#     print(f"JOGADA {i+1}")
+#     print("---------------------------------------------------------------------------------")
+    
 #     jogada()
 #     mostraTabuleiro(tabuleiro, fila_jogador, casa_jogador)
 #     t = input("aperte enter para rodar um dado (\"q\" para sair)")
